5b657202a0cdc17c37964fd3c09f8936b3495814
Igor Mandic <igx33@live.com>
2017-11-21 20:09:21 +0100

    Some work

--- ProjekatTim6/Tim6/PhoneReleaseApiPlugin/PhoneReleaseApi/kod/ucitavanjeTelefona.py
+++ ProjekatTim6/Tim6/PhoneReleaseApiPlugin/PhoneReleaseApi/kod/ucitavanjeTelefona.py
@@ -1,10 +1,12 @@
 
 #from ProjekatTim6.models import Element, Link
-
+import json as jsx
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+import requests
 
 #from ProjekatTim6.Tim6.Core.ProjekatTim6.services.ucitavanje import UcitatiService
+
 from Core.ProjekatTim6.services.ucitavanje import UcitatiService
 
 
@@ -23,13 +25,14 @@ class UcitavanjeTelefona(UcitatiService):
 
 def main():
 
-    url = 'https://fonoapi.freshpixl.com/v1/getlatest'
-    post_data = {'brand':'samsung', 'limit':99, 'token':'40431b4a3ee010cb9a655a999123054b356fc8777ae56c70'}
+    url = "https://fonoapi.freshpixl.com/v1/getlatest"
+    post_data = {"brand":"samsung", "token":"40431b4a3ee010cb9a655a999123054b356fc8777ae56c70"}
+    json_send_data = jsx.dumps(post_data)
+    request = Request(url,json_send_data)
+    #json = urlopen(request).read().decode()
 
-    request = Request(url,urlencode(post_data).encode())
-    json = urlopen(request).read().decode()
 
-    print(json)
+    print(urlopen(request).read())
 
 if __name__ == "__main__":
     main()
